91e018fab8e6de868a6cb654d544c15815d1c739
yuriilevkovych <levkovych.yurii@pdffiller.team>
2019-10-18 16:40:06 +0300

    Feedback

--- frontend/views/page/lt/news.php
+++ frontend/views/page/lt/news.php
@@ -32,28 +32,7 @@ $this->title = 'Maramax';
     <div class="feed">
         <div class="wrap">
             <span class="_title">Contact us</span>
-            <div class="form">
-                <p>YOU CAN REQUEST MISCALCULATIONS ORDER, USE THE FEEDBACK FORM. AND ALSO, CAN REQUEST A CALL AND OUR, MANAGER, YOU WILL BE CONTACTED.</p>
-                <div role="form" class="wpcf7" id="wpcf7-f1073-o2" lang="ru-RU" dir="ltr">
-                    <div class="screen-reader-response"></div>
-                    <form name="" action="/en/news/#wpcf7-f1073-o2" method="post" class="wpcf7-form" novalidate="novalidate">
-                        <div style="display: none;">
-                            <input type="hidden" name="_wpcf7" value="1073">
-                            <input type="hidden" name="_wpcf7_version" value="4.2.1">
-                            <input type="hidden" name="_wpcf7_locale" value="ru_RU">
-                            <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f1073-o2">
-                            <input type="hidden" name="_wpnonce" value="439361aa46">
-                        </div>
-                        <div class="row">
-                            <span class="wpcf7-form-control-wrap fio"><input type="text" name="fio" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Ім'я"></span> <span class="wpcf7-form-control-wrap email"><input type="email" name="email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="E-Mail"></span> <span class="wpcf7-form-control-wrap tel"><input type="tel" name="tel" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-required wpcf7-validates-as-tel" aria-required="true" aria-invalid="false" placeholder="Телефон"></span>
-                        </div>
-                        <div class="row">
-                            <span class="wpcf7-form-control-wrap msg"><textarea name="msg" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Ваше повідомлення"></textarea></span>
-                        </div>
-                        <div class="row">
-                            <input type="submit" value="Відправити" class="wpcf7-form-control wpcf7-submit">
-                        </div>
-                        <div class="wpcf7-response-output wpcf7-display-none"></div></form></div>    </div>
+                <?= CallbackWidget::widget() ?>
         </div>
     </div>
     <div id="top">UP</div>    </section>

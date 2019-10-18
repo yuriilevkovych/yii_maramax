f658639c4e0c34a3bafa2795897f88046ed8e779
yuriilevkovych <levkovych.yurii@pdffiller.team>
2019-10-18 19:13:06 +0300

    Feedback

--- frontend/views/post/ru/cardboard.php
+++ frontend/views/post/ru/cardboard.php
@@ -147,28 +147,9 @@ $this->title = 'Maramax';
     <div class="feed">
         <div class="wrap">
             <span class="_title">Свяжитесь с нами</span>
-            <div class="form">
-                <p>ВЫ МОЖЕТЕ ОСТАВИТЬ ЗАЯВКУ НА ПРОСЧЕТ ЗАКАЗА,ВОСПОЛЬЗОВАВШИСЬ ФОРМОЙ ОБРАТНОЙ СВЯЗИ. А ТАК ЖЕ, МОЖЕТЕ ОСТАВИТЬ ЗАЯВКУ НА ЗВОНОК И НАШИ, МЕНЕДЖЕРЫ, С ВАМИ СВЯЖУТСЯ.</p>
-                <div role="form" class="wpcf7" id="wpcf7-f1073-o2" lang="ru-RU" dir="ltr">
-                    <div class="screen-reader-response"></div>
-                    <form name="" action="/ru/post/micro-corrugated-cardboard/#wpcf7-f1073-o2" method="post" class="wpcf7-form" novalidate="novalidate">
-                        <div style="display: none;">
-                            <input type="hidden" name="_wpcf7" value="1073">
-                            <input type="hidden" name="_wpcf7_version" value="4.2.1">
-                            <input type="hidden" name="_wpcf7_locale" value="ru_RU">
-                            <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f1073-o2">
-                            <input type="hidden" name="_wpnonce" value="439361aa46">
-                        </div>
-                        <div class="row">
-                            <span class="wpcf7-form-control-wrap fio"><input type="text" name="fio" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Ім'я"></span> <span class="wpcf7-form-control-wrap email"><input type="email" name="email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="E-Mail"></span> <span class="wpcf7-form-control-wrap tel"><input type="tel" name="tel" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-required wpcf7-validates-as-tel" aria-required="true" aria-invalid="false" placeholder="Телефон"></span>
-                        </div>
-                        <div class="row">
-                            <span class="wpcf7-form-control-wrap msg"><textarea name="msg" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Ваше повідомлення"></textarea></span>
-                        </div>
-                        <div class="row">
-                            <input type="submit" value="Відправити" class="wpcf7-form-control wpcf7-submit">
-                        </div>
-                        <div class="wpcf7-response-output wpcf7-display-none"></div></form></div>    </div>
+
+            <?= CallbackWidget::widget() ?>
+
         </div>
     </div>
     <div id="top" style="display: none;">ВВЕРХ</div>    </section>
--- frontend/views/post/uk/pacaging-is.php
+++ frontend/views/post/uk/pacaging-is.php
@@ -2,6 +2,8 @@
 
 /* @var $this yii\web\View */
 
+use app\components\CallbackWidget;
+
 $this->title = 'Maramax';
 ?>
 
@@ -28,28 +30,9 @@ $this->title = 'Maramax';
     <div class="feed">
         <div class="wrap">
             <span class="_title">Зв'яжіться з нами</span>
-            <div class="form">
-                <p>ВИ МОЖЕТЕ ЗАЛИШИТИ ЗАЯВКУ НА ПРОРАХУНОК ЗАМОВЛЕННЯ, СКОРИСТАВШИСЬ ФОРМОЮ ЗВОРОТНЬОГО ЗВ'ЯЗКУ. А ТАК САМО, МОЖЕТЕ ЗАЛИШИТИ ЗАЯВКУ НА ДЗВІНОК І НАШІ, МЕНЕДЖЕРИ, ЗВ'ЯЖУТЬСЯ З ВАМИ.</p>
-                <div role="form" class="wpcf7" id="wpcf7-f1073-o2" lang="ru-RU" dir="ltr">
-                    <div class="screen-reader-response"></div>
-                    <form name="" action="/publications/%d0%b8%d0%b7%d0%b3%d0%be%d1%82%d0%be%d0%b2%d0%bb%d0%b5%d0%bd%d0%b8%d0%b5-%d1%83%d0%bf%d0%b0%d0%ba%d0%be%d0%b2%d0%ba%d0%b8-2/#wpcf7-f1073-o2" method="post" class="wpcf7-form" novalidate="novalidate">
-                        <div style="display: none;">
-                            <input type="hidden" name="_wpcf7" value="1073">
-                            <input type="hidden" name="_wpcf7_version" value="4.2.1">
-                            <input type="hidden" name="_wpcf7_locale" value="ru_RU">
-                            <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f1073-o2">
-                            <input type="hidden" name="_wpnonce" value="439361aa46">
-                        </div>
-                        <div class="row">
-                            <span class="wpcf7-form-control-wrap fio"><input type="text" name="fio" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Ім'я"></span> <span class="wpcf7-form-control-wrap email"><input type="email" name="email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="E-Mail"></span> <span class="wpcf7-form-control-wrap tel"><input type="tel" name="tel" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-required wpcf7-validates-as-tel" aria-required="true" aria-invalid="false" placeholder="Телефон"></span>
-                        </div>
-                        <div class="row">
-                            <span class="wpcf7-form-control-wrap msg"><textarea name="msg" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Ваше повідомлення"></textarea></span>
-                        </div>
-                        <div class="row">
-                            <input type="submit" value="Відправити" class="wpcf7-form-control wpcf7-submit">
-                        </div>
-                        <div class="wpcf7-response-output wpcf7-display-none"></div></form></div>    </div>
+
+            <?= CallbackWidget::widget() ?>
+
         </div>
     </div>
     <div id="top">ВГОРУ</div>    </section>
